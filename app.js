2cb3993c742090dc153bd637b4d28b8861b87d20
kevke1990 <otten.kevin@gmail.com>
2026-08-28 13:52:25 +0200

    feat: use Homey OAuth2 app lifecycle for Aqara

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const Homey = require('homey');
+const { OAuth2App } = require('homey-oauth2app');
+const AqaraOAuth2Client = require('./lib/aqara-oauth2-client');
 
-class AqaraApp extends Homey.App {
-  async onInit() {
-    this.log('Aqara App has been initialized');
+class AqaraApp extends OAuth2App {
+  static OAUTH2_CLIENT = AqaraOAuth2Client;
+  static OAUTH2_DEBUG = false;
+  static OAUTH2_MULTI_SESSION = false;
+  static OAUTH2_DRIVERS = ['camera'];
+
+  async onOAuth2Init() {
+    this.log('Aqara OAuth2 App initialized');
   }
 }
 
