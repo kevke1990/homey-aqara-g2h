c7da1a9cd783c3c2851c483b5e6a6fbe80bdd555
kevke1990 <otten.kevin@gmail.com>
2026-08-28 12:44:26 +0200

    fix: use SDK v3 onPairListDevices for stable pairing

--- drivers/camera/driver.js
+++ drivers/camera/driver.js
@@ -1,27 +1,19 @@
 'use strict';
 
 const Homey = require('homey');
-const crypto = require('crypto');
 
 class AqaraCameraDriver extends Homey.Driver {
   async onInit() {
     this.log('AqaraCameraDriver initialized');
   }
 
-  async onPair(session) {
-    session.setHandler('list_devices', async () => [{
+  async onPairListDevices() {
+    return [{
       name: 'Aqara Camera',
-      data: { id: `aqara-${crypto.randomUUID()}` },
-      settings: {
-        camera_name: 'Aqara Camera',
-        rtsp_url: '',
-        aqara_region: 'eu',
-        aqara_client_id: '',
-        aqara_client_secret: '',
-        aqara_access_token: '',
-        aqara_subject_id: '',
+      data: {
+        id: `aqara-camera-${Date.now()}`,
       },
-    }]);
+    }];
   }
 }
 
