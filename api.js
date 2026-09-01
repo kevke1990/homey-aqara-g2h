2f2bb70e38138cecd10e7bd3a0ae0fb0d6c056fd
kevke1990 <otten.kevin@gmail.com>
2026-09-01 09:16:34 +0200

    feat: expose Aqara account authorization endpoints

--- api.js
+++ api.js
@@ -4,4 +4,20 @@ module.exports = {
   async configure({ homey }) {
     return homey.app.configureOAuthFromAppSettings();
   },
+
+  async authCode({ homey, body }) {
+    return { authCodeRequested: true, account: body?.account ? await homey.app.requestAqaraAuthCode({ account: body.account }) : null };
+  },
+
+  async verifyAuthCode({ homey, body }) {
+    return homey.app.verifyAqaraAuthCode({ account: body?.account, authCode: body?.authCode });
+  },
+
+  async status({ homey }) {
+    return homey.app.getAqaraConnectionStatus();
+  },
+
+  async disconnect({ homey }) {
+    return homey.app.disconnectAqaraAccount();
+  },
 };
